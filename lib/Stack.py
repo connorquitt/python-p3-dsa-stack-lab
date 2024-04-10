d477b7e125e3d9a7e68fc14365e25396140dca30
connorq <connorquitt@gmail.com>
2024-04-10 13:49:48 -0500

    all tests passing

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,40 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        if self.size() == 0:
+            return True
+        else:
+            return False
 
     def push(self, item):
-        pass
+        if self.full() == False:
+            self.items.append(item)
 
     def pop(self):
-        pass
+        if self.isEmpty() == True:
+            return None
+        else:
+           return self.items.pop()
 
     def peek(self):
         pass
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return self.size() == self.limit
 
     def search(self, target):
-        pass
+        location = 0
+        if target in self.items:
+            for num in reversed(self.items):
+                if num == target:
+                    return location
+                location += 1
+        else:
+            return -1
